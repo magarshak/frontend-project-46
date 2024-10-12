89892aa3cdf840e200d6b7d7def424febb183d23
Denis Magarshak <d.magarshak@gmail.com>
2024-10-13 01:03:31 +0300

    fix description

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,7 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .description('CLI to some JavaScript string utilities')
+  .description('Compares two configuration files and shows a difference.')
   .version('0.8.0')
   .parse();
 
