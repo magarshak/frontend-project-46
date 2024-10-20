b68bb422cd3a2ba2138de0f3dfa890ce4a4315ac
Denis Magarshak <d.magarshak@gmail.com>
2024-10-20 18:26:22 +0300

    add option at gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,8 +3,10 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .description('Compares two configuration files and shows a difference.')
   .version('0.8.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filePath1> <filePath2>')
   .parse();
 
 // console.log('Welcome to the Diff Tool!');
