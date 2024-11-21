dc0db7045602fcf0327b6f017f64f0359b576e94
Denis Magarshak <d.magarshak@gmail.com>
2024-11-21 21:28:09 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,11 @@
+import path from 'node:path';
 import gendiff from '../src/index.js';
 import result from '../__fixtures__/result.js';
-import path from 'node:path';
 
 const getFixturePath = (filename) => path.join('__fixtures__', filename);
 
 test('check json plain', () => {
-   expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(result);
+  expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(result);
 });
 
 /*
--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const gendiff = (filePath1, filePath2) => {
   // Reading the files by the received paths
   const file1 = readFileSync(path1, 'utf-8');
   const file2 = readFileSync(path2, 'utf-8');
-  
+
   // JSON parsing
   const data1 = JSON.parse(file1);
   const data2 = JSON.parse(file2);
